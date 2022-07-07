5c61508868cfffa3015e73ca1ea33d05c0da0388
Kevin Lewis <klewis@nowsecure.com>
2022-07-07 08:02:53 -0500

    write insecure file

--- app/src/main/java/com/android/example/filelocker/MainActivity.kt
+++ app/src/main/java/com/android/example/filelocker/MainActivity.kt
@@ -17,10 +17,24 @@
 package com.android.example.filelocker
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.android.example.filelocker.databinding.ActivityMainBinding
+import java.io.File
+import java.io.FileReader
 
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.io.*
+import java.net.*
+import java.util.*
+import java.util.zip.ZipFile
 
 /**
  * MainActivity to show a list of encrypted files as well as options to set a master password
@@ -30,9 +44,48 @@ import com.android.example.filelocker.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        arbitraryFile()
         super.onCreate(savedInstanceState)
+
         DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main).apply {
+
             lifecycleOwner = this@MainActivity
+
         }
+
+
     }
+
+    private fun arbitraryFile() {
+        try {
+
+            var execFile = File(getExternalFilesDir("storage"), "insecure.js")
+
+            getExternalFilesDir("storage")?.setExecutable(true, false)
+            getExternalFilesDir("storage")?.setReadable(true, false)
+            getExternalFilesDir("storage")?.setWritable(true, false)
+            execFile.createNewFile()
+            execFile.writeText(
+                "function hello(){" +
+                        "return \"Hello World\";" +
+                        "}"
+            )
+            execFile.setExecutable(true, false)
+            execFile.setWritable(true, false)
+            execFile.setReadable(true, false)
+
+            Log.e("Insecure File written","success")
+
+
+        }
+        catch (e : Exception){
+            Log.e("error", "could not download/run file")
+        }
+
+    }
+
+
 }
+
+
+
